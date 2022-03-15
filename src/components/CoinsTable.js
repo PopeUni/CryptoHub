4e66c6450adfd4412061a3ca020a5c3fb1e41b0a
Jacob Metcalfe <jakemetty@hotmail.com>
2022-03-15 20:11:52 +1100

    fixed search bar issue

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -71,8 +71,8 @@ const CoinsTable = () => {
     const handleSearch = () => {
         return coins.filter(
           (coin) =>
-            coin.name.toLowerCase().includes(search) ||
-            coin.symbol.toLowerCase().includes(search)
+            coin.name.toLowerCase().includes(search.toLowerCase()) ||
+            coin.symbol.toLowerCase().includes(search.toLowerCase())
         );
       };
     const useStyles = makeStyles(() =>  ({
